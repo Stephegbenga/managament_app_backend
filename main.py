12c593f7753bae78660ff91b33da00f3f1ef0f80
Stephegbenga <adegbamiyestephen2018@gmail.com>
2024-06-10 10:04:59 +0100

    fixed path miswrote

--- main.py
+++ main.py
@@ -137,9 +137,10 @@ def orders_create():
 
             for _ in range(quantity):
                 product = Products.find_one({"name": product_name, "is_sold": False})
-                send_sms_message(phone_no, product['file_url'])
-                Products.update_one({"_id": product['_id']}, {"$set": {"is_sold": True, "sold_date": sold_date, "selling_price": selling_price}})
-            Product_names.update_one({"name": product_name}, {"$set": {"selling_price": selling_price}}, upsert=True)
+                if product:
+                    send_sms_message(phone_no, product['file_url'])
+                    Products.update_one({"_id": product['_id']}, {"$set": {"is_sold": True, "sold_date": sold_date, "selling_price": selling_price}})
+                    Product_names.update_one({"name": product_name}, {"$set": {"selling_price": selling_price}}, upsert=True)
 
     return {"status":"success"}
 
